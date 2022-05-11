a4ac4b5e64a9631c91b22a93688fd5cbff6de0b9
zhaosong <zsongzhaop@gmail.com>
2022-05-11 11:24:46 +0800

    feat(geometry): add text

--- src/geometry/shape.js
+++ src/geometry/shape.js
@@ -9,3 +9,17 @@ export function circle(renderer, coordinate, {
     cx: px, cy: py, r, ...styles,
   });
 }
+
+export function text(renderer, coordinate, {
+  x, y, rotate, text, ...styles
+}) {
+  const [px, py] = coordinate([x, y]);
+  renderer.save();
+  renderer.translate(px, py);
+  renderer.rotate(rotate);
+  const textElement = renderer.text({
+    text, x: 0, y: 0, ...styles,
+  });
+  renderer.restore();
+  return textElement;
+}
